624b57e2d0577f67d17c89d7dfa5fc579e100547
Steven Sloan <stevenosloan@gmail.com>
2017-08-29 13:42:46 -0400

    feat(): setup html tempting to allow relative loading from dist dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
-var path = require('path')
+var path    = require('path')
 var webpack = require('webpack')
+
+var HtmlWebpackPlugin         = require('html-webpack-plugin')
+var ExtractTextPlugin         = require("extract-text-webpack-plugin")
+var HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin')
 
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'index.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Brew",
+      template: "src/layout/layout.ejs",
+      hash: true,
+      showErrors: true,
+      xhtml: true,
+      alwaysWriteToDisk: true
+    }),
+    new HtmlWebpackHarddiskPlugin()
+  ],
   module: {
     rules: [
       {
@@ -15,13 +29,9 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
             'scss': 'vue-style-loader!css-loader!sass-loader',
             'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
-          // other vue-loader options go here
         }
       },
       {
@@ -44,6 +54,7 @@ module.exports = {
     }
   },
   devServer: {
+    contentBase: path.join(__dirname, "dist"),
     historyApiFallback: true,
     noInfo: true
   },
