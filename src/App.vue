d40afd624b4a5d14a8121b0e948fdeaa99b2e654
Steven Sloan <stevenosloan@gmail.com>
2017-08-31 10:54:25 -0400

    restyle a bit and add tabbed interface

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,44 @@
 <template>
   <div id="app">
-    <form class="col span1of1 pt3">
-      <fieldset class="ptb3 form-fieldset mb3">
-        <label class="body mt0 mb1">Original Specific Gravity</label>
-        <input class="form-input mb3" type="number" step="0.001" v-model.number="og_specific_gravity" />
+    <nav class="tab-nav ptb3">
+      <a href="#input" :class="input_active" v-on:click.prevent="display('input')">Input</a>
+      <a href="#output" :class="output_active" v-on:click.prevent="display('output')">Output</a>
+    </nav>
 
-        <label class="body mt0 mb1">Original Temperature</label>
-        <input class="form-input" pattern="\d*" type="number" v-model.number="og_temperature" />
-      </fieldset>
+    <div :class="input_tabclass" id="input">
+      <form class="col span1of1">
+        <fieldset class="ptb3 form-fieldset mb3">
+          <label class="body mt0 mb1">Original Specific Gravity</label>
+          <input class="form-input mb3" type="number" step="0.001" v-model.number="og_specific_gravity" />
 
-      <fieldset class="ptb3 form-fieldset">
-        <label class="body mt0 mb1">Final Specific Gravity</label>
-        <input class="form-input mb3" type="number" step="0.001"  v-model.number="fn_specific_gravity" />
+          <label class="body mt0 mb1">Original Temperature</label>
+          <input class="form-input" pattern="\d*" type="number" v-model.number="og_temperature" />
+        </fieldset>
 
-        <label class="body mt0 mb1">Final Temperature</label>
-        <input class="form-input" pattern="\d*" type="number" v-model.number="fn_temperature" />
-      </fieldset>
-    </form>
+        <fieldset class="ptb3 form-fieldset">
+          <label class="body mt0 mb1">Final Specific Gravity</label>
+          <input class="form-input mb3" type="number" step="0.001"  v-model.number="fn_specific_gravity" />
 
-    <hr class="mtb3" />
+          <label class="body mt0 mb1">Final Temperature</label>
+          <input class="form-input" pattern="\d*" type="number" v-model.number="fn_temperature" />
+        </fieldset>
+      </form>
+    </div>
 
-    <div class="col span1of1 pb3">
-      <h2 class="heading center">Results</h2>
+    <div :class="output_tabclass" id="output">
+      <div class="col span1of1">
 
-      <dl class="dl">
-        <dt class="subheading">ABV</dt>
-        <dd class="body mb3">{{ abv }}%</dd>
+        <fieldset class="ptb3 form-fieldset mb3">
+          <label class="body mt0 mb1">ABV</label>
+          <div class="form-input mb3">{{ abv }}%</div>
 
-        <dt class="subheading">Estimated Calories <small class="small">/ 12 oz.</small></dt>
-        <dd class="body mb3">{{ calories }}</dd>
+          <label class="body mt0 mb1">Estimated Calories <small class="small">/ 12 oz.</small></label>
+          <div class="form-input mb3">{{ calories }}</div>
 
-        <dt class="subheading">Real Attenuation</dt>
-        <dd class="body">{{ real_attenuation }}</dd>
-      </dl>
+          <label class="body mt0 mb1">Real Attenuation</label>
+          <div class="form-input mb3">{{ real_attenuation }}%</div>
+        </fieldset>
+      </div>
     </div>
   </div>
 </template>
@@ -46,7 +52,8 @@ export default {
       og_specific_gravity: 1.05,
       og_temperature: 78,
       fn_specific_gravity: 1.00,
-      fn_temperature: 78
+      fn_temperature: 78,
+      active_tab: "input"
     }
   },
 
@@ -65,6 +72,18 @@ export default {
     },
     real_attenuation () {
       return this.round(((this.og - 1) - (this.fg - 1)) / (this.og - 1) * 81.4, 1);
+    },
+    output_tabclass () {
+      return this.tabClass('output');
+    },
+    input_tabclass () {
+      return this.tabClass('input');
+    },
+    input_active () {
+      return this.active_tab === "input" ? "active" : "";
+    },
+    output_active () {
+      return this.active_tab === "output" ? "active" : "";
     }
   },
 
@@ -77,6 +96,12 @@ export default {
     },
     round (number, places = 0) {
       return Math.round(Math.pow(10, places) * number) / Math.pow(10, places);
+    },
+    display (name) {
+      this.active_tab = name;
+    },
+    tabClass (name) {
+      return `tab ${this.active_tab === name ? 'tab-active' : 'tab-hidden'}`;
     }
   }
 }
